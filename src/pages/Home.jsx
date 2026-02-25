4019454161717cb16dff452deaeee4ba84770f27
Josh Schoeman <joshuaschoemana7772002777a@gmail.com>
2026-02-25 21:33:17 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
           >
             <div className="flex items-center gap-2 mb-4">
               <img 
-                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/699b110a371758e340a213c3/8808e415f_Refine_the_existing_Lensly_wor_Nano_Banana_Pro_05232.jpg" 
+                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/699b110a371758e340a213c3/9c7cb5e90_1-014a4b80.png" 
                 alt="Lensly Logo" 
                 className="h-8"
               />
